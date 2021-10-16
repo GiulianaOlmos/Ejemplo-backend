fab9b9c849bf7f1c8aa295ca26f312691a005f9f
Milagros Zea <nzea@frc.utn.edu.ar>
2021-10-16 15:41:27 -0300

    api usuarios - en clase

--- services.mocked/users.mocked.service.js
+++ services.mocked/users.mocked.service.js
@@ -35,32 +35,6 @@ usersServiceMocked.add = async (req, res) => {
 };
 
 usersServiceMocked.modify = (req, res) => {
-    users = [
-        {
-            id: 1,
-            username: 'webcampmet',
-            password: 'webcampmet',
-            name: 'webcampmet nombre',
-            age: 32,
-            active: true
-        },
-        {
-            id: 2,
-            username: 'webcampmet2',
-            password: 'webcampmet2',
-            name: 'webcampmet2 nombre',
-            age: 24,
-            active: true
-        },
-        {
-            id: 3,
-            username: 'webcampmet1',
-            password: 'webcampmet1',
-            name: 'webcampmet1 nombre',
-            age: 16,
-            active: true
-        }
-    ];
     try {
         console.log('+++++++++++++++++++++++++++++++++++');
         console.log('body', req.body);
